52d55fb9531a77724d5a9f853fc4573c0e6b320e
yugul <yugul.sharma@gmail.com>
2017-12-19 22:19:24 +0530

    final code

--- resources/JS/script.js
+++ resources/JS/script.js
@@ -1,150 +1,101 @@
-
 //---------------------------------------------------------------------------------
 //                        Sticky Navigation
 //---------------------------------------------------------------------------------
-$(document).ready(function(){
+$(document).ready(function () {
 
 
-    $('.js--expertise').waypoint(function(direction){
+    $('.js--expertise').waypoint(function (direction) {
 
-        if(direction == "down"){
+        if (direction == "down") {
             $('nav').addClass('sticky');
-        }
-        else{
+        } else {
             $('nav').removeClass('sticky');
         }
 
-    },
-      {
+    }, {
         offset: '60px;'
     });
 
 
 
+    //---------------------------------------------------------------------------------
+    //                        scroll to section
+    //---------------------------------------------------------------------------------
 
+    $('.js--scroll-to-contact').click(function () {
 
-    $('.js--scroll-to-contact').click(function(){
-
-        $('html, body').animate({scrollTop: $('.js--contact').offset().top-35}, 1500);
-
-    });
-
-
-    $('.js--scroll-to-expertise').click(function(){
-
-        $('html, body').animate({scrollTop: $('.js--expertise').offset().top-45}, 1000);
+        $('html, body').animate({
+            scrollTop: $('.js--contact').offset().top - 35
+        }, 1500);
 
     });
 
 
-    $('.js--scroll-to-header').click(function(){
+    $('.js--scroll-to-expertise').click(function () {
 
-        $('html, body').animate({scrollTop: $('header').offset().top}, 1000);
+        $('html, body').animate({
+            scrollTop: $('.js--expertise').offset().top - 45
+        }, 1000);
+
+    });
+
+
+    $('.js--scroll-to-header').click(function () {
+
+        $('html, body').animate({
+            scrollTop: $('header').offset().top
+        }, 1000);
 
     });
 
 
 
-    $('.js--scroll-to-skills').click(function(){
+    $('.js--scroll-to-skills').click(function () {
 
-        $('html, body').animate({scrollTop: $('.js--skills').offset().top}, 1000);
+        $('html, body').animate({
+            scrollTop: $('.js--skills').offset().top
+        }, 1000);
 
     });
 
 
-    $('.js--scroll-to-projects').click(function(){
+    $('.js--scroll-to-projects').click(function () {
 
-        $('html, body').animate({scrollTop: $('.js--projects').offset().top}, 1000);
+        $('html, body').animate({
+            scrollTop: $('.js--projects').offset().top
+        }, 1000);
 
     });
 
 
 });
 
-
-$(document).scroll(function() {
+/*
+$(document).scroll(function () {
     var cutoff = $(window).scrollTop();
 
-    $('.sticky .main-nav li').each(function(){
-        if($(this).offset().top + $(this).height() > cutoff){
+    $('.sticky .main-nav li').each(function () {
+        if ($(this).offset().top + $(this).height() > cutoff) {
             $('.sticky .main-nav li').removeClass('current');
             $(this).addClass('current');
             return false; // stops the iteration after the first one on screen
         }
     });
 });
-
-
+*/
 
 //---------------------------------------------------------------------------------
 //                        Progress Bar
 //---------------------------------------------------------------------------------
-/*
-$(window).scroll(function() {
-    var hT = $('.skills').offset().top,
-        hH = $('.skills').outerHeight(),
-        wH = $(window).height(),
-        wS = $(this).scrollTop();
-    console.log((hT-wH) , wS);
-    if (wS > (hT+hH-wH)){
-        $('#html').LineProgressbar({
-            percentage:90,
-            radius: '3px',
-            height: '20px',
-            fillBackgroundColor: '#FF0044',
-            ShowProgressCount: false,
-            duration: 1100
-        });
 
-        $('#css').LineProgressbar({
-            percentage:80,
-            radius: '3px',
-            height: '20px',
-            fillBackgroundColor: '#FF0044',
-            ShowProgressCount: false,
-            duration: 1150
-        });
-        $('#jscript').LineProgressbar({
-            percentage:70,
-            radius: '3px',
-            height: '20px',
-            fillBackgroundColor: '#FF0044',
-            ShowProgressCount: false,
-            duration: 1200
-        });
-        $('#jq').LineProgressbar({
-            percentage:75,
-            radius: '3px',
-            height: '20px',
-            fillBackgroundColor: '#FF0044',
-            ShowProgressCount: false,
-            duration: 1250
-        });
-        $('#photoshop').LineProgressbar({
-            percentage:80,
-            radius: '3px',
-            height: '20px',
-            fillBackgroundColor: '#FF0044',
-            ShowProgressCount: false,
-            duration: 1300
-
-        });
-
-
-    }
-});
-
-*/
-
-
-$(document).scroll(function() {
+$(document).scroll(function () {
     var cutoff = $(window).scrollTop();
 
-    $('section').each(function(){
-        if($(this).offset().top + $(this).height() > cutoff){
+    $('section').each(function () {
+        if ($(this).offset().top + $(this).height() > cutoff) {
 
             $('#html').LineProgressbar({
-                percentage:90,
+                percentage: 90,
                 radius: '3px',
                 height: '20px',
                 fillBackgroundColor: '#FF0044',
@@ -153,7 +104,7 @@ $(document).scroll(function() {
             });
 
             $('#css').LineProgressbar({
-                percentage:80,
+                percentage: 80,
                 radius: '3px',
                 height: '20px',
                 fillBackgroundColor: '#FF0044',
@@ -161,7 +112,7 @@ $(document).scroll(function() {
                 duration: 1150
             });
             $('#jscript').LineProgressbar({
-                percentage:70,
+                percentage: 70,
                 radius: '3px',
                 height: '20px',
                 fillBackgroundColor: '#FF0044',
@@ -169,7 +120,7 @@ $(document).scroll(function() {
                 duration: 1200
             });
             $('#jq').LineProgressbar({
-                percentage:75,
+                percentage: 75,
                 radius: '3px',
                 height: '20px',
                 fillBackgroundColor: '#FF0044',
@@ -177,7 +128,7 @@ $(document).scroll(function() {
                 duration: 1250
             });
             $('#photoshop').LineProgressbar({
-                percentage:80,
+                percentage: 80,
                 radius: '3px',
                 height: '20px',
                 fillBackgroundColor: '#FF0044',
@@ -186,7 +137,6 @@ $(document).scroll(function() {
 
             });
 
-
             return false; // stops the iteration after the first one on screen
         }
     });
@@ -194,22 +144,24 @@ $(document).scroll(function() {
 
 
 //---------------------------------------------------------------------------------
-//                        Active state in sticky nav
+//                     Show Active state in sticky nav
 //--------------------------------------------------------------------------------
 var lastId,
     topMenu = $(".main-nav"),
-    topMenuHeight = topMenu.outerHeight()+15,
+    topMenuHeight = topMenu.outerHeight() + 15,
     // All list items
     menuItems = topMenu.find("a"),
     // Anchors corresponding to menu items
-    scrollItems = menuItems.map(function(){
+    scrollItems = menuItems.map(function () {
         var item = $($(this).attr("href"));
-        if (item.length) { return item; }
+        if (item.length) {
+            return item;
+        }
     });
-
+//For smooth scroll that we alresdy using above by .animate
 // Bind click handler to menu items
 // so we can get a fancy scroll animation
- /*   menuItems.click(function(e){
+/*   menuItems.click(function(e){
         var href = $(this).attr("href"),
             offsetTop = href === "#" ? 0 : $(href).offset().top-topMenuHeight+1;
         $('html, body').stop().animate({
@@ -219,17 +171,17 @@ var lastId,
     });
 */
 // Bind to scroll
-$(window).scroll(function(){
+$(window).scroll(function () {
     // Get container scroll position
-    var fromTop = $(this).scrollTop()+topMenuHeight;
+    var fromTop = $(this).scrollTop() + topMenuHeight;
 
     // Get id of current scroll item
-    var cur = scrollItems.map(function(){
+    var cur = scrollItems.map(function () {
         if ($(this).offset().top < fromTop)
             return this;
     });
     // Get the id of the current element
-    cur = cur[cur.length-1];
+    cur = cur[cur.length - 1];
     var id = cur && cur.length ? cur[0].id : "";
 
     if (lastId !== id) {
@@ -237,16 +189,14 @@ $(window).scroll(function(){
         // Set/remove active class
         menuItems
             .parent().removeClass("active")
-            .end().filter("[href='#"+id+"']").parent().addClass("active");
+            .end().filter("[href='#" + id + "']").parent().addClass("active");
     }
 });
 
-
-
-
+//---------------------------------------------------------------------------------
+//                      To prevent  "right-click"
+//--------------------------------------------------------------------------------
 /*
-// To prevent  "right-click"
-
 $('html').bind('contextmenu', function(e) {
   //  window.alert('All Rights Reserved \u00A9');
 
@@ -261,65 +211,4 @@ $(document).bind('keydown keypress', 'ctrl+s', function () {
 });
 
 
-*/
-
-
-
-
-/*
-
-//---------------------------------------------------------------------------------
-//                        Typing effect
-//---------------------------------------------------------------------------------
-
-
-$(function(){
-    $(".write").typed({
-        strings: ["user interfaces.","landing pages.", "web design.","web applications."],
-        typeSpeed: 50,
-        loop: true,
-    });
-});
-
-
-
-
-
- strings: ["^1000 User interfaces","^1000 landing pages", "^1000 Web design","^1000 Web applications"],
-
-$(function(){
-    $(".typed").typed({
-        strings: ["Developers.", "Designers.", "People."],
-        // Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
-        stringsElement: null,
-        // typing speed
-        typeSpeed: 30,
-        // time before typing starts
-        startDelay: 1200,
-        // backspacing speed
-        backSpeed: 20,
-        // time before backspacing
-        backDelay: 500,
-        // loop
-        loop: true,
-        // false = infinite
-        loopCount: 5,
-        // show cursor
-        showCursor: false,
-        // character for cursor
-        cursorChar: "|",
-        // attribute to type (null == text)
-        attr: null,
-        // either html or text
-        contentType: 'html',
-        // call when done callback function
-        callback: function() {},
-        // starting callback function before each string
-        preStringTyped: function() {},
-        //callback for every typed string
-        onStringTyped: function() {},
-        // callback for reset
-        resetCallback: function() {}
-    });
-});
 */
